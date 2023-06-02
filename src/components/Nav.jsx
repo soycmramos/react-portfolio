c2ca10f520f85977c7588c0a2964c2f0d91bc136
Carlos Mario Ramos Pérez <carlosmarioramos34@gmail.com>
2023-06-02 16:15:29 -0500

    Centre Nav's items

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -11,11 +11,11 @@ const Nav = () => {
 			</label>
 
 			<div className="fixed top-14 inset-0 p-4 w-3/5 sm:w-1/2 bg-white shadow-sm md:shadow-none md:w-auto md:static md:p-0 -translate-x-full md:translate-x-0 transition">
-				<ul className="flex flex-col gap-2 md:flex-row item">
-					<li><a href="#" className="block w-full h-full py-2 px-4 rounded hover:bg-slate-100 font-semibold transition">Item 1</a></li>
-					<li><a href="#" className="block w-full h-full py-2 px-4 rounded hover:bg-slate-100 font-semibold transition">Item 2</a></li>
-					<li><a href="#" className="block w-full h-full py-2 px-4 rounded hover:bg-slate-100 font-semibold transition">Item 3</a></li>
-					<li><a href="#" className="block w-full h-full py-2 px-4 rounded hover:bg-slate-100 font-semibold transition">Item 4</a></li>
+				<ul className="flex flex-col gap-2 md:flex-row items-center">
+					<li><a href="#" className="block w-full self-center py-2 px-4 rounded hover:bg-slate-100 font-semibold transition">Item 1</a></li>
+					<li><a href="#" className="block w-full self-center py-2 px-4 rounded hover:bg-slate-100 font-semibold transition">Item 2</a></li>
+					<li><a href="#" className="block w-full self-center py-2 px-4 rounded hover:bg-slate-100 font-semibold transition">Item 3</a></li>
+					<li><a href="#" className="block w-full self-center py-2 px-4 rounded hover:bg-slate-100 font-semibold transition">Item 4</a></li>
 					<li>
 						<button className='h-full p-3 hover:bg-slate-100 rounded-full transition cursor-pointer'>
 							<BsMoonStars className='w-5 h-5' />
