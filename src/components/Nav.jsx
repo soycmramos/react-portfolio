7ddc46caa097da8edfb5b8dc7bd6c7fff088d653
Carlos Mario Ramos Pérez <carlosmarioramos34@gmail.com>
2023-06-03 15:00:59 -0500

    Replace input for button in Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,11 @@
 const Nav = () => {
-
 	return (
 		<nav>
-			<input type="checkbox" id="hamburger" className="peer" hidden />
-			<label htmlFor="hamburger" className="flex flex-col place-content-around hover:bg-zinc-300 p-2 w-10 h-10 transition rounded cursor-pointer md:hidden">
+			<button className="flex flex-col place-content-around hover:bg-zinc-300 p-3 w-12 h-12 transition-all rounded-full cursor-pointer md:hidden">
 				<span className="block h-0.5 w-full bg-zinc-700 rounded transition"></span>
 				<span className="block h-0.5 w-full bg-zinc-700 rounded transition"></span>
 				<span className="block h-0.5 w-full bg-zinc-700 rounded transition"></span>
-			</label>
-
+			</button>
 			<div className="fixed top-14 inset-0 p-4 w-3/5 sm:w-1/2 bg-white shadow-sm md:shadow-none md:w-auto md:static md:p-0  transition">
 				<ul className="flex flex-col gap-2 md:flex-row md:items-center">
 					<li><a href="#" className="btn btn-default md:btn-outline-primary w-full text-left">Button</a></li>
