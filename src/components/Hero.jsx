4e72d6e2a52cfdb1a9f926438acfc9bf6b25bf67
Carlos Mario Ramos Pérez <carlosmarioramos34@gmail.com>
2023-06-02 23:56:31 -0500

    Se applt for button component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,24 @@
 const Hero = () => {
-  return (
-    <section className="bg-zinc-100 py-12">
-      <div className="container mx-auto px-4">
-        <div className="flex flex-col md:flex-row-reverse items-center gap-8">
-          <img
-            src="http://via.placeholder.com/640x360"
-            alt="Hero img"
-            className="md:w-1/2 rounded-lg "
-          />
-          <div className="md:w-1/2 md:mt-0 flex flex-col gap-4 md:gap-6">
-            <h2 className="text-zinc-700 font-bold text-3xl lg:text-5xl">Lorem ipsum dolor sit</h2>
-            <p className="text-zinc-700">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil dignissimos odit blanditiis. Placeat iste commodi eaque quibusdam similique saepe, voluptatibus delectus, autem provident iusto magni.
-            </p>
-            {/* <a href="#" className="inline-block py-2 px-4 rounded font-semibold text-zinc-100 bg-zinc-700 hover:bg-zinc-600 transition cursor-pointer self-start">Lorem</a> */}
-            <a href="#" className="inline-block py-2 px-4 rounded font-semibold text-white bg-teal-500 hover:bg-teal-600 transition cursor-pointer self-start">Lorem</a>
-          </div>
-        </div>
-      </div>
-    </section>
-  )
+	return (
+		<section className="bg-zinc-100 py-12">
+			<div className="container mx-auto px-4">
+				<div className="flex flex-col md:flex-row-reverse items-center gap-8">
+					<img
+						src="http://via.placeholder.com/640x360"
+						alt="Hero img"
+						className="md:w-1/2 rounded-lg "
+					/>
+					<div className="md:w-1/2 md:mt-0 flex flex-col gap-4 md:gap-6">
+						<h2 className="text-zinc-700 font-bold text-3xl lg:text-5xl">Lorem ipsum dolor sit</h2>
+						<p className="text-zinc-700">
+							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil dignissimos odit blanditiis. Placeat iste commodi eaque quibusdam similique saepe, voluptatibus delectus, autem provident iusto magni.
+						</p>
+						<button className="btn btn-primary">Button</button>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
 }
 
 export default Hero
